96db5bd1960a63a5f9dfea6ee7423c2f324077f5
jeunghwansuh <jeunghwansuh@gmail.com>
2018-08-07 19:00:22 +0700

    Improved layout organization

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import kivy
+kivy.require('1.10.1')
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 
 
-class GridLayoutApp(App):
+class GridLayout2(GridLayout):
+    pass
 
+class CustomLayoutApp(App):
     def build(self):
-        return GridLayout()
+        return GridLayout2()
 
-app = GridLayoutApp()
+app = CustomLayoutApp()
 app.run()
