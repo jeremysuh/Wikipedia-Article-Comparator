38f34603193401f207e89bde8051a8254460a270
jeunghwansuh <jeunghwansuh@gmail.com>
2018-08-07 21:50:16 +0700

    Fixed AsyncImage issues by configuring python settings

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import kivy
-kivy.require('1.10.1')
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image, AsyncImage
 
 import wikipedia
+
 
 class GridLayout2(GridLayout):
     pass
 
 class CustomLayoutApp(App):
     def build(self):
+
+
         return GridLayout2()
+
+
 
 app = CustomLayoutApp()
 app.run()
