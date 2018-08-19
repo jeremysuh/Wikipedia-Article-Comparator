9b254cb93d63ea6092478483faac1dfef1825537
jeunghwansuh <jeunghwansuh@gmail.com>
2018-08-19 23:27:10 +0900

    Added initial custom layout

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import kivy
 
 from kivy.app import App
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image, AsyncImage
+from kivy.uix.textinput import TextInput
 
 import wikipedia
 
 
-class GridLayout2(GridLayout):
+class MainLayout(GridLayout):
+
+    label_wid = ObjectProperty()
+
+    def change_text(self, num):
+        print "fire"
+        if num == 5:
+            self.ids.sixbutton.text = 'woter'
+            self.ids.firstpic.source = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Wendy_Son_at_Dream_Concert_on_May_12%2C_2018.jpg/800px-Wendy_Son_at_Dream_Concert_on_May_12%2C_2018.jpg"
+        else:
+            self.ids.sixbutton.text = 'fore'
+
+
     pass
 
-class CustomLayoutApp(App):
+class WikipediaComparatorApp(App):
     def build(self):
 
-
-        return GridLayout2()
-
+        return MainLayout()
 
 
-app = CustomLayoutApp()
+
+app = WikipediaComparatorApp()
 app.run()
